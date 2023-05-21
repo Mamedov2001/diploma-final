a7e85e4f0d863e06e57cec1bdb945a8862757c35
Mamedov2001 <mamedov.zaur001@gmail.com>
2023-05-21 23:48:08 +0600

    clear code

--- src/test/java/kz/careerguidance/applicationapi/ApplicationApiApplicationTests.java
+++ src/test/java/kz/careerguidance/applicationapi/ApplicationApiApplicationTests.java
@@ -1,13 +1,12 @@
 package kz.careerguidance.applicationapi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
